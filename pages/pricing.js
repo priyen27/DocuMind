385727219657d961b43eb9230f341e299a11b7b7
priyen27 <priyenmehta27@gmail.com>
2025-09-06 14:23:51 +0530

    Removed razor pay related code

--- pages/pricing.js
+++ pages/pricing.js
@@ -31,17 +31,17 @@ export default function Pricing() {
     }
   }, [user]);
 
-  useEffect(() => {
-    // Load Razorpay script
-    const script = document.createElement('script');
-    script.src = 'https://checkout.razorpay.com/v1/checkout.js';
-    script.async = true;
-    document.body.appendChild(script);
+  // useEffect(() => {
+  //   // Load Razorpay script
+  //   const script = document.createElement('script');
+  //   script.src = 'https://checkout.razorpay.com/v1/checkout.js';
+  //   script.async = true;
+  //   document.body.appendChild(script);
     
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   };
+  // }, []);
 
   const fetchUserData = async () => {
     try {
